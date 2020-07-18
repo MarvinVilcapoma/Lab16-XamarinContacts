a79c89515d2e9a332c8df13723fd780b89bc897b
Friman Vilcapoma <friman.vilcapoma@tecsup.edu.pe>
2020-07-17 23:16:43 -0500

    Commit Vilcapoma

---
+++ TodoREST/Constants.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace TodoREST
+{
+    public static class Constants
+    {
+        // URL of REST service
+        //public static string RestUrl = Device.RuntimePlatform == Device.Android ? "https://10.0.2.2:5001/api/todoitems/{0}" : "https://localhost:5001/api/todoitems/{0}";
+        // API REST HECHO EN NODEJS + MYSQL
+        public static string RestUrl = "https://appeople.herokuapp.com/todoapi/api/people/{0}";
+    }
+}
---
+++ TodoREST/Views/TodoItemPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Xamarin.Forms;
+using Acr.UserDialogs;
+using System.Threading.Tasks;
+
+namespace TodoREST
+{
+	public partial class TodoItemPage : ContentPage
+	{
+		bool isNewItem;
+
+		public TodoItemPage (bool isNew = false)
+		{
+			InitializeComponent ();
+			isNewItem = isNew;
+		}
+
+		async void OnSaveButtonClicked (object sender, EventArgs e)
+		{
+			UserDialogs.Instance.ShowLoading("Registrando Contacto");			 
+			var todoItem = (TodoItem)BindingContext;
+			await Task.Delay(3000);
+			UserDialogs.Instance.HideLoading();
+			await DisplayAlert("Contacto Agregado", "El contacto ha sido agregado exitosamente", "Ok");
+			await App.TodoManager.SaveTaskAsync (todoItem, isNewItem);
+			await Navigation.PopAsync ();
+		}
+
+		async void OnDeleteButtonClicked (object sender, EventArgs e)
+		{
+			var todoItem = (TodoItem)BindingContext;
+			await App.TodoManager.DeleteTaskAsync (todoItem);
+			await Navigation.PopAsync ();
+		}
+
+		async void OnCancelButtonClicked (object sender, EventArgs e)
+		{
+			await Navigation.PopAsync ();
+		}
+	}
+}
